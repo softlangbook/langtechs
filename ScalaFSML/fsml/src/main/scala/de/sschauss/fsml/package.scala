91b54865bc2468d66b2ef0d886112f2692e78c64
Simon Schauß <sschauss@uni-koblenz.de>
2015-02-24 21:34:59 +0100

    removed unused code

--- ScalaFSML/fsml/src/main/scala/de/sschauss/fsml/package.scala
+++ ScalaFSML/fsml/src/main/scala/de/sschauss/fsml/package.scala
@@ -1,66 +1,47 @@
 package de.sschauss
 
-import scala.reflect.macros.whitebox
+import scala.language.implicitConversions
 import scala.language.experimental.macros
+import scala.reflect.macros.whitebox
 
 package object fsml {
 
-
   type Name = String
 
+  object state {
+    val initial = false
 
-  def state(x: Transition): State = macro stateImpl
+    def apply(x: Transition): State = macro stateImpl
 
-  def stateImpl(c: whitebox.Context)(x: c.Tree) = {
-    import c.universe._
-    val q"..$stats" = x
-    q"State(false, $stats)"
-  }
-
-  object initial {
-    def state(x: Transition): State = macro stateImpl
-
-    def stateImpl(c: whitebox.Context)(x: c.Tree) = {
+    def stateImpl(c: whitebox.Context)(x: c.Tree) : c.universe.Tree = {
       import c.universe._
       val q"..$stats" = x
-      q"State(true, $stats)"
+      q"""State("id",$initial, $stats)"""
     }
   }
 
-  /**
   object initial {
+    val initial = true
 
-    def state(x: State): State = macro initialImpl
-    def initialImpl(c: whitebox.Context)(x: c.Tree) = {
+    def state(x: Transition): State = macro stateImpl
+
+    def stateImpl(c: whitebox.Context)(x: c.Tree) : c.universe.Tree = {
       import c.universe._
-      val q"de.sschauss.fsml.`package`.State.apply($_, $transitions)" = x
-      q"State(true, $transitions)"
+      val q"..$stats" = x
+      q"""State("id",$initial, $stats)"""
     }
   }
-    */
-
 
   case class Fsm(states: List[State])
 
-  case class State(initial: Boolean, transitions: List[Transition])
-
-  case class Transition(input: Input, action: Option[Action], to: Option[Id]) {
-    val -> : Id => Transition = to => Transition(input, action, Some(to))
-    val / : Action => Transition = action => Transition(input, Some(action), to)
+  case class State(id: String, initial: Boolean, transitions: List[Transition]) {
   }
 
-  case class Id(name: Name)
-
-  case class Action(name: Name)
-
-  case class Input(name: Name)
+  case class Transition(input: String, action: Option[String], to: Option[String]) {
+    lazy val -> : String => Transition = to => Transition(input, action, Some(to))
+    lazy val / : String => Transition = action => Transition(input, Some(action), to)
+  }
 
   implicit def stringToTransition(id: String): Transition = Transition(id, None, None)
-
-  implicit def stringToAction(id: String): Action = Action(id)
-
-  implicit def stringToId(id: String): Id = Id(id)
-
-  implicit def stringToInput(id: String): Input = Input(id)
 
 }
