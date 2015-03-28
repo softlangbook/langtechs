59317e7fd2a7676f5b624f0d81f3e5b88365c5c0
Simon Schauß <sschauss@uni-koblenz.de>
2015-03-28 17:49:11 +0100

    added clojure project, renamed projects

---
+++ clojure-fsml/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-fsml
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-fsml/test/clojure_fsml/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-fsml.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-fsml.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
