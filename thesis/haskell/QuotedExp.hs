29d3b011b7e9cc246257d4fdad3477771306de5d
Simon Schauß <sschauss@uni-koblenz.de>
2015-09-17 20:11:40 +0200

    removed unused haskell source code

--- thesis/haskell/QuotedExp.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module QuotedExp where
-
-import Language.Haskell.TH
-
-quotedExp :: ExpQ
-quotedExp = [| (1, [2, 3]) |]
--- thesis/haskell/SplicedExp.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module SplicedExp where
-
-import QuotedExp
-
-splicedExp :: Num a => (a, [a])
-splicedExp = $quotedExp
